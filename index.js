2d72d59a4d08c7d0e8e45e2eac915be41780a312
Bukkiedevs <bukkie92@gmail.com>
2023-01-22 20:45:25 +0100

    Added the IDs javascript

--- index.js
+++ index.js
@@ -20,6 +20,13 @@ function showCurrent(response) {
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.name;
+  let conditionElement = document.querySelector("#condition");
+  conditionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  console.log(response.data);
 }
 
 let apiKey = "a33b693cfbefd271b0ed075f9a8f65f0";
