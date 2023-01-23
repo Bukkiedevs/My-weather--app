e57aa9ad7dd9a2f37a083f4c748d87514eda9432
Bukkiedevs <bukkie92@gmail.com>
2023-01-23 15:27:51 +0100

    Added the search engine JavaScript

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ let minute = currentData.getMinutes();
 if (minute < 10) {
   minute = `0${minute}`;
 }
-let season = document.querySelector("#season");
-season.innerHTML = `${day} ${hour}:${minute}`;
+let date = document.querySelector("#date");
+date.innerHTML = `${day} ${hour}:${minute}`;
 
 function showCurrent(response) {
   let temperatureElement = document.querySelector("#temperature");
@@ -27,14 +27,34 @@ function showCurrent(response) {
   let conditionElement = document.querySelector("#condition");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let iconElement = document.querySelector("#icon");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   conditionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute(
+    "alt",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].description}@2x.png`
+  );
+}
+function search(city) {
+  let apiKey = "a33b693cfbefd271b0ed075f9a8f65f0";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showCurrent);
 }
 
-let apiKey = "a33b693cfbefd271b0ed075f9a8f65f0";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Stockholm&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(showCurrent);
+search("Stockholm");
+
+function handleSearch(event) {
+  event.preventDefault();
+  let inputElement = document.querySelector("#input-In");
+  search(inputElement.value);
+}
+let form = document.querySelector("#search-form");
+addEventListener("submit", handleSearch);
